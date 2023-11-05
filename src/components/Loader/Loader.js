6b3ecd75e9392020d00be5b05324fd6156e4cb81
Rushabh Mulraj Shah <rushabhmshah@gofynd.com>
2023-11-05 19:37:10 +0530

    Changes - Adding prop-types for validation - Adding conditional logic for display - Optimisation of code and CSS

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,23 +1,58 @@
 /* Package imports */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 /* Style imports */
 import styles from './Loader.scss';
 
-const Loader = () => {
-    let imageClasses = [styles.imageWrapper, styles.shimmer];
-    let textClasses = [styles.textShimmer, styles.shimmer];
+const LoaderProps = {
+    count: PropTypes.number,
+    displayImages: PropTypes.bool,
+    displayText: PropTypes.bool,
+    multipleImages: PropTypes.bool
+}
 
-    return (
-        <div className={styles.loaderWrapper}>
-            <div className={imageClasses.join(' ')}></div>
-            <div className={styles.textWrapper}>
+const Loader = (props) => {
+    /* Classes initialisation */
+    let imageClasses = [styles.imageElement, styles.shimmer];
+    let textClasses = [styles.textElement, styles.shimmer];
+
+    /* Elements initialisation */
+    let imageShimmer = null;
+    let textShimmer = null;
+
+    if(props.displayImages) {
+        imageShimmer = (
+            <div className={styles.imageWrapper}>
+                <div className={imageClasses.join(' ')} />
+            </div>
+        );
+    }
+
+    if(props.displayText) {
+        textShimmer = (
+             <div className={styles.textWrapper}>
                 <div className={textClasses.join(' ')} />
                 <div className={textClasses.join(' ')} />
                 <div className={textClasses.join(' ')} />                
             </div>
+        )
+    }
+
+    return (
+        <div className={styles.loaderWrapper}>
+            { imageShimmer }
+            { textShimmer }
         </div>
     )
+}
+
+Loader.propTypes = LoaderProps;
+Loader.defaultProps = {
+    count: 1,
+    displayImages: true,
+    displayText: true,
+    multipleImages: false
 }
 
 export default Loader;
